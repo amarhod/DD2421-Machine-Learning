4937fa2abb1100ee8ed230640e65db4a8fa918c2
Amar Hodzic <amar_hodzic@hotmail.com>
2021-03-17 21:41:45 +0100

    feat: Trains with XGBoost and evaluates the final set

--- challenge/main.py
+++ challenge/main.py
@@ -3,18 +3,26 @@ from classifiers import *
 from plotting import *
 
 
-def convert_to_txt(df_train_features, df_train_labels):
-    df_train_features = pd.get_dummies(df_train_features, columns=['x6', 'x12'])
-    df_train_features.to_csv(r'challenge_X.txt', header=None, index=None, sep=',', mode='a')
-    df_train_labels.to_csv(r'challenge_Y.txt', header=None, index=None, sep=',', mode='a')
-
 def main():
-    (df_train_features, df_train_labels) = read_and_clean_data()
-    #decision_tree(df_train_features, df_train_labels)
-    #convert_to_txt(df_train_features, df_train_labels)
-    naive_bayes(df_train_features, df_train_labels)
-    #support_vector_machine(df_train_features, df_train_labels)
+    (df_train_features, df_train_labels, label_encoder) = read_and_clean_data()
+    X_train, X_test, y_train, y_test = train_test_split(df_train_features, df_train_labels)
 
+    (X_validation, y_validation, label_encoder_V) = read_validation_data()
+
+    #CLASSIFIERS
+    #decision_tree(X_train, X_test, y_train, y_test)
+    #naive_bayes(X_train, X_test, y_train, y_test)
+    #support_vector_machine(X_train, X_test, y_train, y_test)
+    #rf = random_forest(X_train, X_test, y_train, y_test)
+    #voting(X_train, X_test, y_train, y_test)
+    xgb = xgboost(X_train, X_test, y_train, y_test)
+
+    #PREDICTION AND SAVING
+    X_EVALUATE = read_evaluation_data()
+    y_predict = xgb.predict(X_EVALUATE)
+    y_predict_string_labels = label_encoder_V.inverse_transform(y_predict)
+    df_predicted_labels = pd.DataFrame(y_predict_string_labels)
+    save_prediction_to_txt(df_predicted_labels)
 
 
 if __name__ == '__main__':
